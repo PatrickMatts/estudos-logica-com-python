8248707532db581e9d21333067cecfc78defa8ac
Patrick Matthew <patrikksouza04@gmail.com>
2025-01-16 13:23:08 +0000

    Novas sugestões

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,10 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+
+#variavel para uma string
+minha_string = "Frutas"
+
+#variavel para uma lista de strings
+minha_lista = ["Maçã", "banana", "laranja", "uva"]
+
+print(minha_string)
+print(minha_lista)
